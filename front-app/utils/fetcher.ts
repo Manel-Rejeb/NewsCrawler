c89c5e77bc5429978766a4406b951023514d503e
Manel Rejeb <91155171+Manel-Rejeb@users.noreply.github.com>
2025-02-19 22:43:38 +0100

    creating documentation for fetcher file

--- front-app/utils/fetcher.ts
+++ front-app/utils/fetcher.ts
@@ -1,6 +1,12 @@
 import axios, { type AxiosInstance } from 'axios'
 
+/**
+ * @description Axios instance configured for making API requests.
+ * It includes a base URL and credentials support.
+ *
+ * @constant {AxiosInstance} fetcher - The Axios instance used for HTTP requests.
+ */
 export const fetcher: AxiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-  withCredentials: true,
+  baseURL: process.env.NEXT_PUBLIC_API_URL, // API base URL from environment variables
+  withCredentials: true, // Enables sending cookies with requests for authentication
 })
